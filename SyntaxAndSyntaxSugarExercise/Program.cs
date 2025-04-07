62af6b5c538674cf2638f75f6ee72c4a92719e50
justin brewer <justinbrewer49@gmail.com>
2025-04-07 15:14:27 -0400

    syntax1

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 18;
+            Console.WriteLine(answer < 9 ? $"{answer} is less then nine" : $"{answer} is greater than or equals to nine");
         }
     }
 }
